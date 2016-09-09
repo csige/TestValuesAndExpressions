7f2940f4bdc56d5846b1eb40dc4ca2b7f20e3065
Gergely Csizy <gergely.csizy@zlense.com>
2016-09-09 09:57:48 +0200

    A simple test of pointer comparison

--- main.cpp
+++ main.cpp
@@ -1,83 +1,51 @@
-#include <QCoreApplication>
-
 #include <iostream>
-#include <boost/shared_ptr.hpp>
-#include <memory>
-
-//Test functions
-class B;
 
 class A
 {
 public:
-	A()
-	{
-	}
+	A() : m_i(0) { }
 
-	~A()
-	{
-		std::cout<<"Destructor of A!"<<std::endl;
-	}
-
-	void setB(B &b)
-	{
-		this->b = boost::shared_ptr<B>(&b);
-	}
-
-	boost::shared_ptr<B> b;
+protected:
+	int m_i;
 };
 
 class B
 {
 public:
-	B(const A& a)//constructor
-	{
-		std::cout<<"ctor called!"<<std::endl;
-	}
+	B() : m_d(0.0) { }
 
-	B& operator =(const A& a){std::cout<<"assignment operator called!"<<std::endl;return *this;}//assignment operator
-
-	operator A(){std::cout<<"typecast operator called!"<<std::endl;return A();}//typecast-operator
-
-	~B()
-	{
-		std::cout<<"Destructor of B!"<<std::endl;
-	}
-
-	void setA(A &a)
-	{
-		this->a = boost::shared_ptr<A>(&a);
-	}
-
-	boost::shared_ptr<A> a;
+protected:
+	double m_d;
 };
 
-
-int main(int argc, char *argv[])
+class C
+	: public A
+	, public B
 {
-	QCoreApplication app(argc, argv);
+public:
+	C() : m_c('a') { }
 
-	A foo;
-	B bar(foo);
-	bar = foo;
-	foo = bar;
+private:
+	char m_c;
+};
+
+int main()
+{
+	C c;
+	A *pa = &c;
+	B *pb = &c;
+
+	const int x = (pa == &c) ? 1 : 2;
+	const int y = (pb == &c) ? 3 : 4;
+	const int z = (reinterpret_cast<char*>(pa) == reinterpret_cast<char*>(pb)) ? 5 : 6;
+
+	std::cout << x << y << z << std::endl;
+	char* a = reinterpret_cast<char*>(pa);
+	char* b = reinterpret_cast<char*>(pb);
+	std::cout<<toascii(*a)<<"       "<<toascii(*b)<<std::endl;
+	int* t = new int(3);
+	int* t2 = new int(3);
+	std::cout << (t == t2) <<std::endl;
 
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
